bebed8fbe302f653bcc476824fb610f126fe201c
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-11 17:36:09 +0700

    setup sequelize

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const {
+  DB_HOSTNAME,
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD
+} = process.env
+
+
+module.exports = {
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": "mysql"
+  }
+}
