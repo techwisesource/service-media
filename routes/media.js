d5d3693bfe4cc013ba365a0ef76eb5ecd8b95adb
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-15 22:28:19 +0700

    create api delete image

--- routes/media.js
+++ routes/media.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
 
 const { Media } = require('../models');
 const { HOSTNAME } = process.env;
@@ -52,5 +53,31 @@ router.post('/', (req, res) => {
 
   })
 });
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if(!media) {
+    return res.status(404).json({status: 'Error Not Found', message: 'Image not found'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if(err) {
+      return res.status(400).json({
+        status: 'error',
+        message: err.message
+      })
+    };
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'Image deleted'
+    })
+  })
+})
 
 module.exports = router;
