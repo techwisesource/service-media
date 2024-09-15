c2a851a6c227928ffb69a1045f7f0b8f898eb090
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-15 22:03:25 +0700

    create api get all image

--- routes/media.js
+++ routes/media.js
@@ -6,6 +6,22 @@ const base64Img = require('base64-img');
 const { Media } = require('../models');
 const { HOSTNAME } = process.env;
 
+router.get('/', async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  })
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
+});
+
 router.post('/', (req, res) => {
   const image = req.body.image;
 
